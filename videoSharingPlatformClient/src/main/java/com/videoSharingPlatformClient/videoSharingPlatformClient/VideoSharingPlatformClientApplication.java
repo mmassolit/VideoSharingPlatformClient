d92eeea5145286df50de057f208dc7f94a10807b
Maxim <mmassolit@gmail.com>
2020-11-16 18:53:41 +0200

    Initial commit

---
+++ videoSharingPlatformClient/src/main/java/com/videoSharingPlatformClient/videoSharingPlatformClient/VideoSharingPlatformClientApplication.java
@@ -0,0 +1,13 @@
+package com.videoSharingPlatformClient.videoSharingPlatformClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoSharingPlatformClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoSharingPlatformClientApplication.class, args);
+	}
+
+}
---
+++ videoSharingPlatformClient/src/test/java/com/videoSharingPlatformClient/videoSharingPlatformClient/VideoSharingPlatformClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videoSharingPlatformClient.videoSharingPlatformClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoSharingPlatformClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
